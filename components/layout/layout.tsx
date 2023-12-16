23db995dae64c68b2ee2c902e03ca2bb1b6eb090
Rene Vucko <rene.vucko@gmail.com>
2023-12-16 15:40:03 +0100

    test

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,28 +1,13 @@
 import styles from '@/styles/Layout.module.css'
 import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
+import Header from '../header/header';
 
 const Layout = ({ children, }: { children: any }) => {
 
     return (
         <div className={styles.parent}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
+            <Header></Header>
             {children}
             <Toolbar>
                 <Typography variant="h6" component="div">

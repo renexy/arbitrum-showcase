5055378707d3347280be1577f08a1c259a0387de
renexy <rene.vucko@gmail.com>
2023-12-24 15:08:58 +0100

    rtest

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -18,87 +18,34 @@ const Header = () => {
         setAnchorElNav(null);
     };
 
-    return (<AppBar position="static" sx={{ backgroundColor: "#eeeeee", paddingLeft: "12px", paddingRight: "12px" }}>
-        <Toolbar disableGutters>
+    return (<AppBar position="static" sx={{ backgroundColor: "#000", paddingLeft: "12px", paddingRight: "12px" }}>
+        <Toolbar sx={{ justifyContent: 'center' }}>
             <Typography
-                variant="h4"
+                variant="h3"
                 noWrap
                 component="a"
                 sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
                     fontFamily: 'monospace',
                     fontWeight: 900,
-                    letterSpacing: '.3rem',
-                    color: '#607d8b',
                     textDecoration: 'none',
+                    display: { xs: 'none', sm: 'block' }
                 }}
             >
-                Burgir
+                WELCOME TO LASHLAB
             </Typography>
-
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleOpenNavMenu}
-                    sx={{ color: "#607d8b" }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorElNav}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                    open={Boolean(anchorElNav)}
-                    onClose={handleCloseNavMenu}
-                    sx={{
-                        display: { xs: 'block', md: 'none' }
-                    }}
-                >
-                    {pages.map((page) => (
-                        <MenuItem key={page} onClick={handleCloseNavMenu}>
-                            <Typography textAlign="center" color="#607d8b">{page}</Typography>
-                        </MenuItem>
-                    ))}
-                </Menu>
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                {/* {pages.map((page: any) => (
-                        <Button
-                            key={page}
-                            onClick={handleCloseNavMenu}
-                            sx={{ my: 2, color: '#000', display: 'block' }}
-                        >
-                            {page}
-                        </Button>
-                    ))} */}
-            </Box>
-
-            <Button variant="contained">
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="a"
-                    sx={{
-                        fontFamily: 'monospace',
-                        fontWeight: 900,
-                        textDecoration: 'none',
-                    }}
-                >
-                    Connect
-                </Typography>
-            </Button>
+            <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                sx={{
+                    fontFamily: 'monospace',
+                    fontWeight: 900,
+                    textDecoration: 'none',
+                    display: { xs: 'block', sm: 'none' }
+                }}
+            >
+                WELCOME TO LASHLAB
+            </Typography>
         </Toolbar>
     </AppBar>
     )
--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -39,6 +39,15 @@ const Layout = ({ children, }: { children: any }) => {
                     }
                 }
             }
+        },
+        breakpoints: {
+            values: {
+                xs: 0,
+                sm: 600,
+                md: 900,
+                lg: 1200,
+                xl: 1536,
+            },
         }
     })
     return (

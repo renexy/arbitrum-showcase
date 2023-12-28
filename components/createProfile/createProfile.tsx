16fa17ec297e261f96ccf41ac5227af4edbdd793
Rene Vucko <rene.vucko@gmail.com>
2023-12-28 20:18:51 +0100

    fix

--- components/createProfile/createProfile.tsx
+++ components/createProfile/createProfile.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { Button, IconButton, InputAdornment, List, ListItem, ListItemText, Paper, TextField, Typography } from '@mui/material';
+import { Button, IconButton, InputAdornment, InputLabel, List, ListItem, ListItemText, Paper, Select, TextField, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import { blueGrey } from '@mui/material/colors';
@@ -39,17 +39,28 @@ export default function CreateProfile() {
             <Typography variant="h5">Create profile</Typography>
             <TextField required label="Profile name" onChange={(e) => { setProfileName(e.target.value) }}
                 id="outlined-basic" variant="outlined" color="secondary" sx={{ 'fieldSet': { border: '1px solid grey' } }} />
-            <TextField
-                required
-                color="secondary"
-                id="outlined-textarea"
-                label="Metadata"
-                onChange={(e) => { setMetaData(e.target.value) }}
-                placeholder="Metadata"
-                multiline
-                sx={{ 'fieldSet': { border: '1px solid grey' } }}
-                rows={4}
-            />
+            <div style={{ display: 'flex', gap: '8px', flexWrap: 'wrap', width: '100%' }}>
+                <Select
+                    defaultValue={1}
+                    sx={{ 'fieldSet': { border: '1px solid grey' } }}
+                    color="secondary"
+                    inputProps={{
+                        name: 'Metadata',
+                        id: 'uncontrolled-native',
+                    }}
+                >
+                    <option value={1}>IPFS</option>
+                </Select>
+                <TextField
+                    required
+                    color="secondary"
+                    id="outlined-textarea"
+                    label="Metadata"
+                    onChange={(e) => { setMetaData(e.target.value) }}
+                    placeholder="Metadata"
+                    sx={{ 'fieldSet': { border: '1px solid grey' }, flex: 1 }}
+                />
+            </div>
             <TextField required label="Owner"
                 onChange={(e) => { setOwner(e.target.value) }}
                 id="outlined-basic" variant="outlined" color="secondary" sx={{ 'fieldSet': { border: '1px solid grey' } }} />

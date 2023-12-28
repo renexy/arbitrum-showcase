0332fc7aa4820f153c58d95eccb59adb94079643
Rene Vucko <rene.vucko@gmail.com>
2023-12-28 18:55:34 +0100

    f

--- components/baseDialog/baseDialog.tsx
+++ components/baseDialog/baseDialog.tsx
@@ -9,9 +9,9 @@ import ListItemText from '@mui/material/ListItemText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import PersonIcon from '@mui/icons-material/Person';
-import { blue } from '@mui/material/colors';
+import { blue, blueGrey } from '@mui/material/colors';
 import AddIcon from '@mui/icons-material/Add';
-import { Fab } from '@mui/material';
+import { Backdrop, CircularProgress, Fab, Typography } from '@mui/material';
 
 const emails = ['username@gmail.com', 'user02@gmail.com'];
 
@@ -24,21 +24,38 @@ export interface SimpleDialogProps {
 function TransactionDialog(props: SimpleDialogProps) {
     const { onClose, selectedValue, open } = props;
 
+    const [confirmTranscation, setConfirmTransaction] = React.useState(false)
+
     const handleClose = () => {
         onClose(selectedValue);
     };
 
-    const handleListItemClick = (value: string) => {
-        onClose(value);
-    };
+    // TODO: DELETE THIS
+    React.useEffect(() => {
+        if (confirmTranscation) {
+            setTimeout(() => {
+                setConfirmTransaction(!confirmTranscation)
+            }, 3000)
+        }
+    }, [confirmTranscation])
 
     return (
-        <Dialog onClose={handleClose} open={open}>
-            <div style={{ padding: '10px', display: 'flex', flexDirection: 'column' }}>
-                <DialogTitle>Confirm transaction</DialogTitle>
-                <Button size="small" variant="contained" color="secondary">Confirm</Button>
-            </div>
-        </Dialog>
+        <>
+            {!confirmTranscation && <Dialog onClose={handleClose} open={open}>
+                <div style={{ padding: '10px', display: 'flex', flexDirection: 'column' }}>
+                    <DialogTitle>Confirm transaction</DialogTitle>
+                    <Button size="small" variant="contained" color="secondary" onClick={() => { setConfirmTransaction(true) }}>Confirm</Button>
+                </div>
+            </Dialog>}
+            {confirmTranscation && <Backdrop
+                sx={{ color: '#fff', display: 'flex', flexDirection: 'column', gap: '8px', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}
+                onClick={handleClose}
+            >
+                <CircularProgress sx={{ color: '#f5f5f5' }} />
+                <Typography variant="h6" color={'#f5f5f5'}>Transaction confirmed</Typography>
+            </Backdrop>}
+        </>
     )
 }
 

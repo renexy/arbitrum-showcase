fee437f893e5dc0b03a5aee52fad110889c9ce7a
heisenburgers <zkdaoorganization@gmail.com>
2023-12-30 18:15:38 +0400

    latest

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -155,9 +155,12 @@ export default function Profile() {
     }
 
     const handleMembersUpdate = async (registry: any, signer: any) => {
-      if (newAddresses.length === 0) {
+      if (newProfileMembers.length === 0) {
+        console.log("HUH")
         return;
       }
+
+      console.log("newProfileMembers", newProfileMembers) 
 
       // we don't want to add the members that exist in selectProfile
       // so we have to take those out!

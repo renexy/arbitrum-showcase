bd84a6e932df50f29aab576fb56b4f0e855b8d6a
renev <rene.vucko@comtradegaming.com>
2024-01-03 15:52:13 +0100

    big push

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -68,33 +68,32 @@ export default function Profile() {
         setMembersToAdd([])
         setMembersToRemove([])
         setSingleMember('')
+        displayPendingOwnerShip(searchOption?.find(x => x.anchor === selectedProfileHash)!)
     }
 
-    const displayPendingOwnerShip = () => {
-        if (!selectedProfile) {
+    const displayPendingOwnerShip = (profile: TransformedProfile) => {
+        if (!profile) {
             console.log("No active selectedProfile");
             return;
         }
-        if (selectedProfile?.pendingOwner === '0x0000000000000000000000000000000000000000') {
+        if (profile?.pendingOwner === '0x0000000000000000000000000000000000000000') {
             setShowPendingOwnership(false)
-            console.log(selectedProfile.pendingOwner)
+            console.log(profile.pendingOwner)
             console.log("false")
         } else {
             setShowPendingOwnership(true)
-            console.log(selectedProfile?.pendingOwner)
+            console.log(profile?.pendingOwner)
             console.log("true")
         }
     }
 
     useEffect(() => {
         setInitialValues()
-        displayPendingOwnerShip()
     }, [selectedProfileHash])
 
     useEffect(() => {
         if (editMode) return
         setInitialValues()
-        displayPendingOwnerShip()
         setItemsChanged(false)
     }, [editMode, selectedProfileHash, userProfiles, userMemberProfiles])
 
@@ -534,7 +533,7 @@ export default function Profile() {
                             variant="standard"
                         />
                         {showPendingOwnership && address === selectedProfile.pendingOwner &&
-                            <Button variant="contained" sx={{ paddingLeft: '8px' }} endIcon={<HandshakeIcon sx={{ fill: '#fff', cursor: 'pointer' }} onClick={() => {setDialogAcceptOwnership(!dialogAcceptOwnership)}} />}>
+                            <Button variant="contained" sx={{ paddingLeft: '8px' }} endIcon={<HandshakeIcon sx={{ fill: '#fff', cursor: 'pointer' }} onClick={() => { setDialogAcceptOwnership(!dialogAcceptOwnership) }} />}>
                                 Accept ownership
                             </Button>}
                     </div>

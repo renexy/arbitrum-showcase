834f54c0cef074d5c1594d24b09f2bffd6858075
Rene Vucko <rene.vucko@gmail.com>
2024-01-05 11:43:20 +0100

    vfv

--- components/pool/pool.tsx
+++ components/pool/pool.tsx
@@ -2,19 +2,22 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-import { Autocomplete, Button, Fab, Paper, TextField, Typography } from '@mui/material';
+import { Autocomplete, Button, Fab, InputAdornment, Paper, TextField, Typography } from '@mui/material';
 import { CreatePoolArgs } from "@allo-team/allo-v2-sdk/dist/Allo/types";
 import { TransactionData } from "@allo-team/allo-v2-sdk/dist/Common/types";
 import { StrategyType } from "@allo-team/allo-v2-sdk/dist/strategies/MicroGrantsStrategy/types";
 import GlobalContext from '@/hooks/context/ContextAggregator';
 import { useNetwork } from 'wagmi';
 import AddIcon from '@mui/icons-material/Add';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import CircleIcon from '@mui/icons-material/Circle';
 import { ethers } from 'ethers'
 import {
   getWalletClient,
   sendTransaction,
   waitForTransaction,
 } from "@wagmi/core";
+import { green, red } from '@mui/material/colors';
 
 const top100Films = [
   { label: 'The Shawshank Redemption', year: 1994 },
@@ -283,10 +286,146 @@ export default function Pool() {
           renderInput={(params) => <TextField {...params} label="Pool" color="secondary" />}
         />
         <Button variant="contained"
-          sx={{ paddingLeft: '8px', width: { xs: '100%', sm: '260px' } }} endIcon={<AddIcon sx={{ fill: '#fff', cursor: 'pointer' }}
+          sx={{ paddingLeft: '8px', fontSize: '1rem', width: { xs: '100%', sm: '260px' } }} endIcon={<AddIcon sx={{ fill: '#fff', cursor: 'pointer' }}
           />}>
           Create new pool
         </Button>
+      </div>
+      <div style={{ display: 'flex', gap: '8px', alignItems: 'center', flexWrap: 'wrap' }}>
+        <TextField
+          id="standard-read-only-input"
+          color='secondary'
+          sx={{ textAlign: 'left', border: 'none' }}
+          value={'Pool 108'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+            disableUnderline: true,
+            sx: {
+              fontSize: '1.5rem',
+              color: 'rgba(0, 0, 0, 0.6)'
+            }
+          }}
+          variant="standard"
+        />
+        <Typography sx={{ color: green[300], fontWeight: 'bold', fontSize: '1.5rem' }}>ACTIVE</Typography>
+      </div>
+      <div style={{ display: 'flex', gap: '18px', flexWrap: 'wrap', width: '100%' }}>
+        <TextField
+          id="standard-read-only-input"
+          label="Strategy type"
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'Manual'}
+          InputProps={{
+            readOnly: true,
+            disabled: true
+          }}
+          variant="standard"
+        >
+        </TextField>
+        <TextField
+          id="standard-read-only-input"
+          label="Website"
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'link'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+          }}
+          variant="standard"
+        >
+        </TextField>
+        <TextField
+          id="standard-read-only-input"
+          label={'Profile ID'}
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'string'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+          }}
+          variant="standard"
+        />
+      </div>
+      <div style={{ display: 'flex', gap: '18px', flexWrap: 'wrap', width: '100%' }}>
+        <TextField
+          id="standard-read-only-input"
+          label={'Pool amount'}
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'number'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+          }}
+          variant="standard"
+        />
+        <TextField
+          id="standard-read-only-input"
+          label={'Max. allocation '}
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'number'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+          }}
+          variant="standard"
+        />
+        <TextField
+          id="standard-read-only-input"
+          label={'Threshold'}
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'number'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+          }}
+          variant="standard"
+        />
+      </div>
+
+      <div style={{ display: 'flex', gap: '18px', flexWrap: 'wrap', width: '100%' }}>
+        <TextField
+          id="standard-read-only-input"
+          label={'Applications'}
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'number'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+          }}
+          variant="standard"
+        />
+        <TextField
+          id="standard-read-only-input"
+          label={'Profile req.'}
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'number'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+          }}
+          variant="standard"
+        />
+        <TextField
+          id="standard-read-only-input"
+          label={'Start date'}
+          color='secondary'
+          sx={{ flex: '1 0 auto', minWidth: '200px' }}
+          value={'number'}
+          InputProps={{
+            readOnly: true,
+            disabled: true,
+          }}
+          variant="standard"
+        />
       </div>
     </Box>
   );

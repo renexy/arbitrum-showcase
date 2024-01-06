d45b28fe9f77bcbe5f86ab7dfaebb787d1e7af02
heisenburgers <zkdaoorganization@gmail.com>
2024-01-06 16:41:44 +0400

    latest

--- components/browsePools/browsePools.tsx
+++ components/browsePools/browsePools.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getActiveMicroGrantsQuery, getEndedMicroGrantsQuery, getUpcomingMicroGrantsQuery, graphqlEndpoint } from "@/queries/poolQuery";
 import { Grid, Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
 import { green } from '@mui/material/colors';
 import GridModuleCss from '@/styles/Grid.module.css'
+import { getIPFSClient } from "@/services/ipfs";
+import { TPoolData, TPoolMetadata } from "@/types/typesPool";
+import request from "graphql-request";
+
+enum TPoolType {
+  UPCOMING = "upcoming",
+  ACTIVE = "active",
+  ENDED = "ended",
+}
 
 const getRandomImage = () => {
     const imageUrls = [
@@ -14,73 +24,138 @@ const getRandomImage = () => {
 };
 
 const getRandomText = () => {
-    const texts = [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        // Add more text options as needed
-    ];
-    return texts[Math.floor(Math.random() * texts.length)];
+  const texts = [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      // Add more text options as needed
+  ];
+  return texts[Math.floor(Math.random() * texts.length)];
 };
 
-const BrowsePools = () => {
-    const cardData = Array.from({ length: 6 }, (_, index) => ({
-        id: index,
-        image: getRandomImage(),
-        text: getRandomText(),
-    }));
+const BrowsePools = async () => {
+  const cardData = Array.from({ length: 6 }, (_, index) => ({
+      id: index,
+      image: getRandomImage(),
+      text: getRandomText(),
+  }));
 
-    return (
-        <Grid container spacing={2}>
-            {cardData.map((item) => (
-                <Grid key={item.id} item xs={12} sm={6} md={4} lg={3}>
-                    <Card sx={{ cursor: 'pointer' }}>
-                        <CardMedia
-                            component="img"
-                            height="125"
-                            image={item.image}
-                            alt="Random"
-                            style={{ objectFit: 'cover' }}
-                        />
-                        <CardContent>
-                            <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-                                <div className={GridModuleCss.colBreak}>
-                                    <Typography sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>actual name</Typography>
-                                    <Button variant="outlined" sx={{ background: green[300] }} color="secondary"
-                                        disabled>
-                                        <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: '1rem' }}>ACTIVE</Typography>
-                                    </Button>
-                                </div>
-                                <div className={GridModuleCss.colBreak}>
-                                    <Typography sx={{ fontWeight: 'bold' }}>Pool amount</Typography>
-                                    <Typography>wan ether</Typography>
-                                </div>
-                                <div className={GridModuleCss.colBreak}>
-                                    <Typography sx={{ fontWeight: 'bold' }}>Max alloc.</Typography>
-                                    <Typography>wan ether</Typography>
-                                </div>
-                                <div className={GridModuleCss.colBreak}>
-                                    <Typography sx={{ fontWeight: 'bold' }}>Start</Typography>
-                                    <Typography>1/5/2024</Typography>
-                                </div>
-                                <div className={GridModuleCss.colBreak}>
-                                    <Typography sx={{ fontWeight: 'bold' }}>End</Typography>
-                                    <Typography>1/6/2025</Typography>
-                                </div>
-                                <div className={GridModuleCss.colBreak}>
-                                    <Typography sx={{ fontWeight: 'bold' }}>Strat. type</Typography>
-                                    <Typography>Manual</Typography>
-                                </div>
-                                <div className={GridModuleCss.colBreak}>
-                                    <Typography>Description</Typography>
-                                </div>
-                            </div>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            ))}
+  const ipfsClient = getIPFSClient();
+
+  const getPools = async (type: TPoolType) => {
+    let pools: TPoolData[] = [];
+
+    let graphqlQuery;
+    let responseObject;
+
+    if (type === TPoolType.UPCOMING) {
+      graphqlQuery = getUpcomingMicroGrantsQuery;
+      responseObject = "upcomingMicroGrants";
+    } else if (type === TPoolType.ACTIVE) {
+      graphqlQuery = getActiveMicroGrantsQuery;
+      responseObject = "activeMicroGrants";
+    } else if (type === TPoolType.ENDED) {
+      graphqlQuery = getEndedMicroGrantsQuery;
+      responseObject = "endedMicroGrants";
+    } else {
+      return pools;
+    }
+
+    try {
+      const response: any = await request(
+        graphqlEndpoint,
+        graphqlQuery,
+        {
+          first: 10,
+          offset: 0
+        }
+      );
+      pools = response[responseObject];
+      for (const pool of pools) {
+        let metadata: TPoolMetadata;
+        try {
+          metadata = await ipfsClient.fetchJson(pool.pool.metadataPointer);
+          pool.pool.metadata = metadata;
+          if (metadata.base64Image) {
+            let poolBanner = await ipfsClient.fetchJson(metadata.base64Image);
+            pool.pool.poolBanner = poolBanner.data;
+          }
+          if (!metadata.name) {
+            metadata.name = `Pool ${pool.poolId}`;
+          }
+        } catch {
+          console.log("IPFS", "Unable to fetch metadata");
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    return pools;
+  }
+ 
+  /*const upcomingPools = await getPools(TPoolType.UPCOMING);
+
+  const activePools = await getPools(TPoolType.ACTIVE);
+  
+  const endedPools = await getPools(TPoolType.ENDED);*/
+  
+  /*useEffect(() => {
+    console.log("upcomingPools", upcomingPools);
+    console.log("activePools", activePools);
+    console.log("endedPools", endedPools);
+  }, []);*/
+
+  return (
+    <Grid container spacing={2}>
+      {cardData.map((item) => (
+        <Grid key={item.id} item xs={12} sm={6} md={4} lg={3}>
+            <Card sx={{ cursor: 'pointer' }}>
+                <CardMedia
+                    component="img"
+                    height="125"
+                    image={item.image}
+                    alt="Random"
+                    style={{ objectFit: 'cover' }}
+                />
+                <CardContent>
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                        <div className={GridModuleCss.colBreak}>
+                            <Typography sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>actual name</Typography>
+                            <Button variant="outlined" sx={{ background: green[300] }} color="secondary"
+                                disabled>
+                                <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: '1rem' }}>ACTIVE</Typography>
+                            </Button>
+                        </div>
+                        <div className={GridModuleCss.colBreak}>
+                            <Typography sx={{ fontWeight: 'bold' }}>Pool amount</Typography>
+                            <Typography>wan ether</Typography>
+                        </div>
+                        <div className={GridModuleCss.colBreak}>
+                            <Typography sx={{ fontWeight: 'bold' }}>Max alloc.</Typography>
+                            <Typography>wan ether</Typography>
+                        </div>
+                        <div className={GridModuleCss.colBreak}>
+                            <Typography sx={{ fontWeight: 'bold' }}>Start</Typography>
+                            <Typography>1/5/2024</Typography>
+                        </div>
+                        <div className={GridModuleCss.colBreak}>
+                            <Typography sx={{ fontWeight: 'bold' }}>End</Typography>
+                            <Typography>1/6/2025</Typography>
+                        </div>
+                        <div className={GridModuleCss.colBreak}>
+                            <Typography sx={{ fontWeight: 'bold' }}>Strat. type</Typography>
+                            <Typography>Manual</Typography>
+                        </div>
+                        <div className={GridModuleCss.colBreak}>
+                            <Typography>Description</Typography>
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
         </Grid>
-    );
+      ))}
+    </Grid>
+  );
 };
 
 export default BrowsePools;
--- queries/poolQuery.tsx
+++ queries/poolQuery.tsx
@@ -1,100 +1,30 @@
-import { toChecksumAddress } from '@/global/functions';
-import { useQuery, gql, ApolloError } from '@apollo/client';
-import { StrategyType } from "@allo-team/allo-v2-sdk/dist/strategies/MicroGrantsStrategy/types";
+import { gql } from "graphql-request";
 
-enum TPoolType {
-  UPCOMING = "upcoming",
-  ACTIVE = "active",
-  ENDED = "ended",
-}
+export const graphqlEndpoint = process.env.NEXT_PUBLIC_GRAPHQL_URL || "";
 
-export type TPoolMetadata = {
-  profileId: `0x${string}`;
-  name: string;
-  website: string;
-  description: string;
-  base64Image?: string;
-};
+export const checkIfRecipientIsIndexedQuery = gql`
+  query checkIfRecipientIsIndexedQuery(
+    $chainId: String!
+    $poolId: String!
+    $recipientId: String!
+  ) {
+    microGrantRecipient(
+      chainId: $chainId
+      poolId: $poolId
+      recipientId: $recipientId
+    ) {
+      recipientId
+    }
+  }
+`;
 
-export type TAllocatedData = {
-  recipientId: `0x${string}`;
-  recipientAddress: `0x${string}`;
-  sender: `0x${string}`;
-  contractAddress: `0x${string}`;
-  contractName: string;
-  chainId: string;
-  blockTimestamp: string;
-  status: string;
-  transactionHash: string;
-};
-
-export type TDistributedData = {
-  recipientId: `0x${string}`;
-  recipientAddress: `0x${string}`;
-  sender: `0x${string}`;
-  contractAddress: `0x${string}`;
-  contractName: string;
-  chainId: string;
-  amount: string;
-  blockTimestamp: string;
-  transactionHash: string;
-};
-
-export type TMicroGrantRecipient = {
-  recipientId: `0x${string}`;
-  recipientAddress: `0x${string}`;
-  requestedAmount: string;
-  metadataPointer: string;
-  blockTimestamp: string;
-  isUsingRegistryAnchor: boolean;
-  status: ApplicationStatus;
-  metadata?: any;
-  applicationBanner?: string;
-};
-
-export type TStrategyType = keyof typeof StrategyType;
-
-type ApplicationStatus = "Accepted" | "Rejected" | "Pending" | "Paid";
-
-export type TPoolData = {
-  poolId: string;
-  chainId: string;
-  strategy: string;
-  allocationStartTime: number;
-  allocationEndTime: number;
-  approvalThreshold: number;
-  maxRequestedAmount: string;
-  blockTimestamp: string;
-  useRegistryAnchor: boolean;
-  pool: {
-    strategy: string;
-    strategyName: string;
-    tokenMetadata: {
-      name?: string;
-      symbol?: string;
-      decimals?: number;
-    };
-    token: `0x${string}`;
-    amount: string;
-    metadataPointer: string;
-    poolBanner: string;
-    metadata: TPoolMetadata;
-    profile: {
-      profileId: `0x${string}`;
-      name: string;
-    };
-  };
-  allocateds: TAllocatedData[];
-  distributeds: TDistributedData[];
-  microGrantRecipients: TMicroGrantRecipient[];
-  strategyType: TStrategyType;
-  // Hat
-  hatId?: number;
-  // Gov
-  gov?: string;
-  minVotePower?: string;
-  snapshotReference?: string;
-};
+export const checkIfPoolIsIndexedQuery = gql`
+  query checkIfPoolIsIndexedQuery($chainId: String!, $poolId: String!) {
+    microGrant(chainId: $chainId, poolId: $poolId) {
+      poolId
+    }
+  }
+`;
 
 const microGrantsQuery = `
   {
@@ -132,79 +62,180 @@ export const getActiveMicroGrantsQuery = gql`
   }
 `;
 
-export function usePools(type: TPoolType) {
-  let allPools = [];
-  let offset = 0;
-  let loadingComplete = false;
+export const getUpcomingMicroGrantsQuery = gql`
+  query getUpcomingMicroGrantsQuery($first: Int!, $offset: Int!) {
+    upcomingMicroGrants(orderBy: BLOCK_TIMESTAMP_DESC, first: $first, offset: $offset)
+      ${microGrantsQuery}
+  }
+`;
 
-  const { loading, error, data, fetchMore } = useQuery(getPoolsQuery, {
-    variables: { first: 100, offset, type },
-    notifyOnNetworkStatusChange: true,
-    onCompleted: (data) => {
-      if (data.pools.length > 0) {
-        allPools = allPools.concat(data.pools);
-        offset += data.pools.length;
-        if (data.pools.length < 100) {
-          loadingComplete = true;
-        } else {
-          fetchMore({ variables: { first: 100, offset, type } });
-        }
-      } else {
-        loadingComplete = true;
-      }
-    },
-    onError: (error: ApolloError) => console.error("Pool Query error:", error),
-  });
+export const getEndedMicroGrantsQuery = gql`
+  query getEndedMicroGrantsQuery($first: Int!, $offset: Int!) {
+    endedMicroGrants(orderBy: BLOCK_TIMESTAMP_DESC, first: $first, offset: $offset)
+      ${microGrantsQuery}
+  }
+`;
 
-  return {
-    loading,
-    error,
-    pools: allPools,
-    loadingComplete, // Indicates if all pools have been loaded
-    fetchMore,
-  };
-}
-
-/*const GET_ALL_POOLS = gql`
-  query GetAllPools($after: ID) {
-    pools(first: 100, after: $after) {
-      id
-      token
+export const getMicroGrantQuery = gql`
+  query getMicroGrantQuery($chainId: String!, $poolId: String!) {
+    microGrant(chainId: $chainId, poolId: $poolId) {
+      poolId
+      chainId
       strategy
+      allocationStartTime
+      allocationEndTime
+      approvalThreshold
+      maxRequestedAmount
+      blockTimestamp
+      pool {
+        tokenMetadata
+        token
+        amount
+      }
     }
   }
 `;
 
-// Fetch All Pools
-export function fetchAllPools() {
-  let allPools = [];
-  let lastPoolID = null;
-  let loadingComplete = false;
-
-  const { loading, error, data, fetchMore } = useQuery(GET_ALL_POOLS, {
-    variables: { after: lastPoolID },
-    notifyOnNetworkStatusChange: true,
-    onCompleted: (data) => {
-      if (data.pools.length > 0) {
-        allPools = allPools.concat(data.pools);
-        lastPoolID = data.pools[data.pools.length - 1].id;
-        if (data.pools.length < 100) {
-          loadingComplete = true;
-        } else {
-          fetchMore({ variables: { after: lastPoolID } });
+export const getMicroGrantsRecipientsQuery = gql`
+  query getMicroGrantsRecipientsQuery($chainId: String!, $poolId: String!) {
+    microGrant(chainId: $chainId, poolId: $poolId) {
+      poolId
+      chainId
+      strategy
+      allocationStartTime
+      allocationEndTime
+      approvalThreshold
+      maxRequestedAmount
+      blockTimestamp
+      useRegistryAnchor
+      pool {
+        strategy
+        strategyName
+        tokenMetadata
+        token
+        amount
+        metadataPointer
+        profile {
+          profileId
+          name
         }
-      } else {
-        loadingComplete = true;
       }
-    },
-    onError: (error: ApolloError) => console.error("Pool Query error:", error),
-  });
+      allocateds {
+        recipientId
+        sender
+        contractAddress
+        contractName
+        chainId
+        status
+        blockTimestamp
+        transactionHash
+      }
+      distributeds {
+        recipientId
+        recipientAddress
+        amount
+        sender
+        contractName
+        contractAddress
+        transactionHash
+        blockNumber
+        blockTimestamp
+        chainId
+      }
+      microGrantRecipients {
+        recipientId
+        recipientAddress
+        requestedAmount
+        metadataPointer
+        blockTimestamp
+        isUsingRegistryAnchor
+        status
+      }
+    }
+  }
+`;
 
-  return {
-    loading,
-    error,
-    pools: allPools,
-    loadingComplete, // Indicates if all pools have been loaded
-    fetchMore,
-  };
-}*/
+export const getMicroGrantRecipientQuery = gql`
+  query getMicroGrantRecipientQuery(
+    $chainId: String!
+    $poolId: String!
+    $recipientId: String!
+  ) {
+    microGrantRecipient(
+      chainId: $chainId
+      poolId: $poolId
+      recipientId: $recipientId
+    ) {
+      microGrant {
+        poolId
+        chainId
+        allocationStartTime
+        allocationEndTime
+        maxRequestedAmount
+        blockTimestamp
+        pool {
+          strategy
+          tokenMetadata
+          token
+          amount
+        }
+        allocateds {
+          recipientId
+          sender
+          contractAddress
+          contractName
+          chainId
+          status
+          blockTimestamp
+          transactionHash
+        }
+        distributeds {
+          recipientId
+          recipientAddress
+          amount
+          sender
+          contractName
+          contractAddress
+          transactionHash
+          blockTimestamp
+          chainId
+        }
+      }
+      sender
+      recipientId
+      recipientAddress
+      requestedAmount
+      metadataPointer
+      blockTimestamp
+      isUsingRegistryAnchor
+      status
+    }
+  }
+`;
+
+export const getProfilesByOwnerQuery = gql`
+  query getProfilesByOwnerQuery($chainId: String!, $owner: String!) {
+    profilesByOwner(chainId: $chainId, owner: $owner) {
+      profileId
+      name
+      owner
+      createdAt
+      anchor
+    }
+  }
+`;
+
+export const getProfile = gql`
+  query getProfile($chainId: String!, $profileId: String!) {
+    profile(chainId: $chainId, profileId: $profileId) {
+      profileId
+      nonce
+      name
+      metadataPointer
+      owner
+      anchor
+      creator
+      createdAt
+    }
+  }
+`;

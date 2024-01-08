76d9ce0e5e6e831c189510b0e6e022d7481387a1
Rene Vucko <rene.vucko@gmail.com>
2024-01-08 21:03:40 +0100

    f

--- components/applicationForm/applicationForm.tsx
+++ components/applicationForm/applicationForm.tsx
@@ -1,10 +1,22 @@
 import Container from '@/components/container/container';
-import { Box, Button, Step, StepButton, Stepper, TextField, Typography } from '@mui/material';
+import { Alert, Box, Button, Snackbar, Step, StepButton, Stepper, TextField, Typography } from '@mui/material';
 import Head from 'next/head'
 import React, { useEffect, useState } from 'react';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { useRouter } from 'next/router';
+import GlobalContext from '@/hooks/context/ContextAggregator';
+import { TPoolData } from '@/types/typesPool';
+import { ethers } from 'ethers';
 
 const steps = ['Basic info', 'Grant info'];
+
+const weiToEth = (weiValue: any) => {
+    if (!weiValue) return "0.0 ETH";
+
+    const ethValue = ethers.utils.formatEther(weiValue);
+
+    return `${ethValue}`;
+};
 
 export default function ApplicationForm() {
     const [activeStep, setActiveStep] = React.useState(0);
@@ -14,6 +26,7 @@ export default function ApplicationForm() {
     const [completed, setCompleted] = React.useState<{
         [k: number]: boolean;
     }>({});
+    const { loading, activePools, endedPools } = React.useContext(GlobalContext);
 
     //Form
     const [name, setName] = useState<string>('')
@@ -24,6 +37,9 @@ export default function ApplicationForm() {
     const [recipientAddress, setRecipientAddress] = useState<string>('')
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const [applyDisabled, setApplyDisabled] = useState(true)
+    const [selectedPool, setSelectedPool] = useState<TPoolData | undefined>(undefined)
+    const [showSnackbar, setShowsnackbar] = useState(false)
+    const router = useRouter()
 
     const handleFileChange = (event: any) => {
         const file = event.target.files[0];
@@ -34,6 +50,22 @@ export default function ApplicationForm() {
         }
     };
 
+    React.useEffect(() => {
+        const { id } = router.query;
+
+        if (id) {
+            if (activePools && activePools.length > 0) {
+                const foundActive = activePools.find(pool => pool.poolId === id);
+                console.log(weiToEth(foundActive?.maxRequestedAmount), "lol")
+                if (foundActive) {
+                    setSelectedPool(foundActive);
+                    return;
+                }
+            }
+            setSelectedPool(undefined);
+        }
+    }, [router.query, activePools]);
+
     useEffect(() => {
         if (name && website && description && email && reqAmount && recipientAddress && selectedFile) {
             setApplyDisabled(false)
@@ -42,143 +74,178 @@ export default function ApplicationForm() {
         }
     }, [name, website, description, email, reqAmount, recipientAddress, selectedFile])
 
+    const handleReqAmountChange = (e: any) => {
+        const enteredValue = e.target.value;
+
+        // Regular expression to allow any decimal number
+        const decimalRegex = /^\d*\.?\d*$/; // Allows any decimal format
+
+        if (enteredValue === '' || decimalRegex.test(enteredValue)) {
+            var enteredAmountConverted = parseFloat(enteredValue)
+            var selectedPoolMaxAmount = parseFloat(weiToEth(selectedPool?.maxRequestedAmount))
+            if (enteredAmountConverted <= selectedPoolMaxAmount)
+                setReqAmount(enteredValue); else {
+                if (showSnackbar) return
+                setShowsnackbar(true)
+                setTimeout(() => {
+                    setShowsnackbar(false)
+                }, 3000)
+            }
+        } else {
+            // Invalid input handling (You can modify this part to suit your needs)
+            console.error('Invalid input. Please enter a valid decimal number.');
+        }
+    };
+
     return (
         <>
-            <Typography variant="h5" sx={{ display: 'flex', alignItems: 'center', margin: '18px 0' }}>
-                Apply for pool
-            </Typography>
-            <Stepper nonLinear activeStep={activeStep} sx={{ width: '100%' }}>
-                {steps.map((label, index) => (
-                    <Step key={label} completed={completed[index]}>
-                        <StepButton color="inherit" onClick={handleStep(index)}>
-                            {label}
-                        </StepButton>
-                    </Step>
-                ))}
-            </Stepper>
-            {activeStep === 0 && <>
-                <TextField
-                    variant="outlined"
-                    color="secondary"
-                    value={name}
-                    size="medium"
-                    placeholder='Allo protocol'
-                    onChange={(e: any) => { setName(e.target.value) }}
-                    sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
-                    label="Name"
-                    InputLabelProps={{
-                        shrink: true
-                    }}
-                >
-                </TextField>
-                <TextField
-                    variant="outlined"
-                    color="secondary"
-                    size="medium"
-                    placeholder='https://website.url'
-                    value={website}
-                    onChange={(e: any) => { setWebsite(e.target.value) }}
-                    sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
-                    label="Website"
-                    InputLabelProps={{
-                        shrink: true
-                    }}
-                >
-                </TextField>
-                <TextField
-                    variant="outlined"
-                    color="secondary"
-                    size="medium"
-                    multiline
-                    rows={4}
-                    placeholder='Description'
-                    value={description}
-                    onChange={(e: any) => { setDescription(e.target.value) }}
-                    sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
-                    label="Description"
-                    InputLabelProps={{
-                        shrink: true
-                    }}
-                >
-                </TextField>
-                <TextField
-                    variant="outlined"
-                    color="secondary"
-                    size="medium"
-                    value={email}
-                    onChange={(e: any) => { setEmail(e.target.value) }}
-                    sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
-                    label="Email"
-                    InputLabelProps={{
-                        shrink: true
-                    }}
-                >
-                </TextField>
-            </>}
-            {activeStep === 1 && <>
-                <TextField
-                    variant="outlined"
-                    color="secondary"
-                    value={reqAmount}
-                    size="medium"
-                    onChange={(e: any) => { setReqAmount(e.target.value) }}
-                    sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
-                    label="Requested amount"
-                    InputLabelProps={{
-                        shrink: true
-                    }}
-                >
-                </TextField>
-                <TextField
-                    variant="outlined"
-                    color="secondary"
-                    size="medium"
-                    value={recipientAddress}
-                    onChange={(e: any) => { setRecipientAddress(e.target.value) }}
-                    sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
-                    label="Recipient address"
-                    InputLabelProps={{
-                        shrink: true
-                    }}
-                >
-                </TextField>
-                <Box sx={{ alignSelf: 'center' }}>
-                    <label htmlFor="file-upload" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <Button
-                            component="span"
-                            variant="contained"
-                            color="secondary"
-                            startIcon={<CloudUploadIcon sx={{ fill: 'white' }} />}
-                        >
-                            Upload banner image
-                        </Button>
-                    </label>
-                    <input
-                        id="file-upload"
-                        type="file"
-                        accept=".png,.jpg,.jpeg" // Accept PNG or JPG files only
-                        style={{ display: 'none' }}
-                        onChange={handleFileChange}
-                    />
-                    {selectedFile && (
-                        <Typography variant="body1" color="textSecondary" sx={{ paddingTop: '12px' }}>
-                            Selected File: {selectedFile ? selectedFile!.name : ''}
-                        </Typography>
-                    )}
-                </Box>
-            </>}
+            {selectedPool && <>
+                <Typography variant="h5" sx={{ display: 'flex', alignItems: 'center', margin: '18px 0' }}>
+                    Apply for pool
+                </Typography>
+                <Stepper nonLinear activeStep={activeStep} sx={{ width: '100%' }}>
+                    {steps.map((label, index) => (
+                        <Step key={label} completed={completed[index]}>
+                            <StepButton color="inherit" onClick={handleStep(index)}>
+                                {label}
+                            </StepButton>
+                        </Step>
+                    ))}
+                </Stepper>
+                {activeStep === 0 && <>
+                    <TextField
+                        variant="outlined"
+                        color="secondary"
+                        value={name}
+                        size="medium"
+                        placeholder='Allo protocol'
+                        onChange={(e: any) => { setName(e.target.value) }}
+                        sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
+                        label="Name"
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                    >
+                    </TextField>
+                    <TextField
+                        variant="outlined"
+                        color="secondary"
+                        size="medium"
+                        placeholder='https://website.url'
+                        value={website}
+                        onChange={(e: any) => { setWebsite(e.target.value) }}
+                        sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
+                        label="Website"
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                    >
+                    </TextField>
+                    <TextField
+                        variant="outlined"
+                        color="secondary"
+                        size="medium"
+                        multiline
+                        rows={4}
+                        placeholder='Description'
+                        value={description}
+                        onChange={(e: any) => { setDescription(e.target.value) }}
+                        sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
+                        label="Description"
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                    >
+                    </TextField>
+                    <TextField
+                        variant="outlined"
+                        color="secondary"
+                        size="medium"
+                        value={email}
+                        onChange={(e: any) => { setEmail(e.target.value) }}
+                        sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
+                        label="Email"
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                    >
+                    </TextField>
+                </>}
+                {activeStep === 1 && <>
+                    <TextField
+                        variant="outlined"
+                        color="secondary"
+                        value={reqAmount}
+                        size="medium"
+                        onChange={(e) => handleReqAmountChange(e)}
+                        sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
+                        label="Requested amount"
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                    >
+                    </TextField>
+                    <TextField
+                        variant="outlined"
+                        color="secondary"
+                        size="medium"
+                        value={recipientAddress}
+                        onChange={(e: any) => { setRecipientAddress(e.target.value) }}
+                        sx={{ width: { xs: '100%', sm: '350px' }, alignSelf: 'center' }}
+                        label="Recipient address"
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                    >
+                    </TextField>
+                    <Box sx={{ alignSelf: 'center' }}>
+                        <label htmlFor="file-upload" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <Button
+                                component="span"
+                                variant="contained"
+                                color="secondary"
+                                startIcon={<CloudUploadIcon sx={{ fill: 'white' }} />}
+                            >
+                                Upload banner image
+                            </Button>
+                        </label>
+                        <input
+                            id="file-upload"
+                            type="file"
+                            accept=".png,.jpg,.jpeg" // Accept PNG or JPG files only
+                            style={{ display: 'none' }}
+                            onChange={handleFileChange}
+                        />
+                        {selectedFile && (
+                            <Typography variant="body1" color="textSecondary" sx={{ paddingTop: '12px' }}>
+                                Selected File: {selectedFile ? selectedFile!.name : ''}
+                            </Typography>
+                        )}
+                    </Box>
+                </>}
 
-            <Button
-                component="span"
-                variant="contained"
-                color="secondary"
-                size="medium"
-                disabled={applyDisabled}
-                sx={{ alignSelf: 'flex-end' }}
-                onClick={() => { alert('apply applicant') }}
-            >
-                Apply for pool
-            </Button>
+                <Button
+                    component="span"
+                    variant="contained"
+                    color="secondary"
+                    size="medium"
+                    disabled={applyDisabled}
+                    sx={{ alignSelf: 'flex-end' }}
+                    onClick={() => { alert('apply applicant') }}
+                >
+                    Apply for pool
+                </Button>
+                <Snackbar
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                    open={showSnackbar}
+                    color="primary"
+                >
+                    <Alert severity="error" sx={{ width: '100%' }}>
+                        Max. requested amount is {weiToEth(selectedPool?.maxRequestedAmount)} ETH!
+                    </Alert>
+                </Snackbar>
+            </>}
+            {!selectedPool && <Typography>Loading...</Typography>}
         </>
     )
 }

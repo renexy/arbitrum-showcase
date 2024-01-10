bdaa8e060be6df85803ada2804a4c6d2f3a543fc
Rene Vucko <rene.vucko@gmail.com>
2024-01-10 22:34:58 +0100

    f

--- components/browseProfiles/browseProfiles.tsx
+++ components/browseProfiles/browseProfiles.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Grid, Card, CardMedia, CardContent, Typography, Button, Stack, Skeleton, Box, Tabs, Tab, TextField, ToggleButtonGroup, ToggleButton, Tooltip, IconButton, Pagination } from '@mui/material';
+import { Grid, Card, CardMedia, CardContent, Typography, Button, Stack, Skeleton, Box, Tabs, Tab, TextField, ToggleButtonGroup, ToggleButton, Tooltip, IconButton, Pagination, Snackbar, Alert } from '@mui/material';
 import { green, red } from '@mui/material/colors';
 import GridModuleCss from '@/styles/Grid.module.css'
 import { TPoolData, TPoolMetadata, TProfile } from "@/types/typesPool";
@@ -14,22 +14,20 @@ import AnchorIcon from '@mui/icons-material/Anchor';
 import HailIcon from '@mui/icons-material/Hail';
 import { fetchProfilesByAnchor, fetchProfilesById, fetchProfilesByName, fetchProfilesByOwner, fetchProfilesByPagination } from '@/queries/profilesQuery';
 import { getIPFSClient } from '@/services/ipfs';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const fallbackImageURL = 'https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Welcome-Banners_12301529202210.jpg';
 
-const weiToEth = (weiValue: any) => {
-    if (!weiValue) return "0.0 ETH";
-
-    const ethValue = ethers.utils.formatEther(weiValue);
-    const truncatedEth = ethValue.slice(0, 5); // Retrieve only the first 5 characters
-
-    return `${truncatedEth} ETH`;
+const copyToClipboard = (text: string) => {
+    if (!text) return
+    navigator.clipboard.writeText(text);
 };
 
 const BrowseProfiles = () => {
     const [value, setValue] = React.useState(0);
     const [search, setSearch] = useState('')
     const [filteredPools, setFilteredPools] = useState<TProfile[]>([])
+    const [showSnackbarCopied, setShowsnackbarCopied] = useState(false)
 
     const { loading, activePools, endedPools } = React.useContext(GlobalContext);
 
@@ -41,7 +39,7 @@ const BrowseProfiles = () => {
         setCurrentPage(value);
         // Here you can also fetch data based on the new page number if needed
     };
-    
+
     const ipfsClient = getIPFSClient();
 
     const { profiles: pagination, refetch: refetchByPagination } = fetchProfilesByPagination(10, 0)
@@ -75,55 +73,55 @@ const BrowseProfiles = () => {
     }, [currentPage, paginatedProfiles]);
 
     const test = async () => {
-      const pointer = "bafybeia4khbew3r2mkflyn7nzlvfzcb3qpfeftz5ivpzfwn77ollj47gqi";
+        const pointer = "bafybeia4khbew3r2mkflyn7nzlvfzcb3qpfeftz5ivpzfwn77ollj47gqi";
 
-      const metadataFetch = await ipfsClient.fetchJson(pointer);
+        const metadataFetch = await ipfsClient.fetchJson(pointer);
 
-      console.log("metadataFetch", metadataFetch)
+        console.log("metadataFetch", metadataFetch)
     }
 
     useEffect(() => {
-      const searchByName = async () => {
-        const response = await refetchByName({ name: search });
-        setCustomSearch(response.data.profiles)
-        //console.log("FETCHED BY NAME", response.data.profiles)
-      }
+        const searchByName = async () => {
+            const response = await refetchByName({ name: search });
+            setCustomSearch(response.data.profiles)
+            //console.log("FETCHED BY NAME", response.data.profiles)
+        }
 
-      const searchByProfileId = async () => {
-        const response = await refetchById({ name: search });
-        setCustomSearch(response.data.profiles)
-        //console.log("FETCHED BY ID", response.data.profiles)
-      }
+        const searchByProfileId = async () => {
+            const response = await refetchById({ name: search });
+            setCustomSearch(response.data.profiles)
+            //console.log("FETCHED BY ID", response.data.profiles)
+        }
 
-      const searchByAnchor = async () => {
-        const response = await refetchByAnchor({ name: search });
-        setCustomSearch(response.data.profiles)
-        //console.log("FETCHED BY ANCHOR", response.data.profiles)
-      }
+        const searchByAnchor = async () => {
+            const response = await refetchByAnchor({ name: search });
+            setCustomSearch(response.data.profiles)
+            //console.log("FETCHED BY ANCHOR", response.data.profiles)
+        }
 
-      const searchByOwner = async () => {
-        const response = await refetchByOwner({ name: search });
-        setCustomSearch(response.data.profiles)
-        //console.log("FETCHED BY OWNER", response.data.profiles)
-      }
+        const searchByOwner = async () => {
+            const response = await refetchByOwner({ name: search });
+            setCustomSearch(response.data.profiles)
+            //console.log("FETCHED BY OWNER", response.data.profiles)
+        }
 
-      if (search.length < 1) return
+        if (search.length < 1) return
 
-      if (alignment === "name") {
-          searchByName()
-      }
+        if (alignment === "name") {
+            searchByName()
+        }
 
-      if (alignment === "profileId") {
-          searchByProfileId()
-      }
+        if (alignment === "profileId") {
+            searchByProfileId()
+        }
 
-      if (alignment === "anchor") {
-          searchByAnchor()
-      }
+        if (alignment === "anchor") {
+            searchByAnchor()
+        }
 
-      if (alignment === "owner") {
-          searchByOwner()
-      }
+        if (alignment === "owner") {
+            searchByOwner()
+        }
     }, [search])
 
     // name | profileId | anchor | owner
@@ -213,13 +211,6 @@ const BrowseProfiles = () => {
                 {(search.length > 0 ? customSearch : paginatedProfiles[currentPage - 1] ? paginatedProfiles[currentPage - 1] : []).map((item) => (
                     <Grid key={'active' + item.anchor} item sx={{ padding: '32px 0' }} xs={12} sm={6} md={4} lg={3}>
                         <Card sx={{ cursor: 'pointer' }}>
-                            {/* <CardMedia
-                                component="img"
-                                height="125"
-                                image={item?. || fallbackImageURL}
-                                alt="Random"
-                                style={{ objectFit: 'cover' }}
-                            /> */}
                             <CardContent>
                                 <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
                                     <div className={GridModuleCss.colBreak}>
@@ -232,27 +223,44 @@ const BrowseProfiles = () => {
                                     </div>
                                     <div className={GridModuleCss.colBreak}>
                                         <Typography sx={{ fontWeight: 'bold' }}>Owner</Typography>
-                                        <Typography>                                            {item?.owner
+                                        <Typography>{item?.owner
                                             ? item.owner.id.length > 11
-                                                ? `${item.owner.id.slice(0, 9)}...`
+                                                ? `${item.owner.id.slice(0, 3)}...${item.owner.id.slice(-3)}`
                                                 : item.owner.id
-                                            : 'No ID'}</Typography>
+                                            : 'No ID'}
+                                            <ContentCopyIcon sx={{ cursor: 'pointer', height: '12px' }}
+                                                onClick={() => {
+                                                    copyToClipboard(item?.name);
+                                                    setShowsnackbarCopied(true); setTimeout(() => { setShowsnackbarCopied(false) }, 3000)
+                                                }} /></Typography>
                                     </div>
                                     <div className={GridModuleCss.colBreak}>
                                         <Typography sx={{ fontWeight: 'bold' }}>Profile ID</Typography>
-                                        <Typography>                                            {item?.id
-                                            ? item.id.length > 11
-                                                ? `${item.id.slice(0, 9)}...`
-                                                : item.id
-                                            : 'No ID'}</Typography>
+                                        <Typography>
+                                            {item?.id
+                                                ? item?.id?.length > 11
+                                                    ? `${item.id.slice(0, 3)}...${item.id.slice(-3)}`
+                                                    : item.id
+                                                : '...'}
+
+                                            <ContentCopyIcon sx={{ cursor: 'pointer', height: '12px' }}
+                                                onClick={() => {
+                                                    copyToClipboard(item?.id);
+                                                    setShowsnackbarCopied(true); setTimeout(() => { setShowsnackbarCopied(false) }, 3000)
+                                                }} /></Typography>
                                     </div>
                                     <div className={GridModuleCss.colBreak}>
                                         <Typography sx={{ fontWeight: 'bold' }}>Metadata</Typography>
-                                        <Typography>                                            {item?.metadata
-                                            ? item.metadata?.pointer?.length > 11
-                                                ? `${item.metadata?.pointer?.slice(0, 9)}...`
-                                                : item.metadata?.pointer
-                                            : 'No ID'}</Typography>
+                                        <Typography>{item?.metadata && item.metadata?.pointer
+                                            ? item.metadata.pointer.length > 11
+                                                ? `${item.metadata.pointer.slice(0, 3)}...${item.metadata.pointer.slice(-3)}`
+                                                : item.metadata.pointer
+                                            : '...'}
+                                            <ContentCopyIcon sx={{ cursor: 'pointer', height: '12px' }}
+                                                onClick={() => {
+                                                    copyToClipboard(item?.metadata?.pointer);
+                                                    setShowsnackbarCopied(true); setTimeout(() => { setShowsnackbarCopied(false) }, 3000)
+                                                }} /></Typography>
                                     </div>
                                 </div>
                             </CardContent>
@@ -261,6 +269,15 @@ const BrowseProfiles = () => {
                 ))}
             </Grid >
 
+            <Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                open={showSnackbarCopied}
+                color="secondary"
+            >
+                <Alert severity="success" sx={{ width: '100%' }}>
+                    Copied to clipboard!
+                </Alert>
+            </Snackbar>
             <Pagination count={10} page={currentPage} onChange={handlePageChange} color="secondary" />
         </>
     );
